a2e9ddcd011ebc1408d43ddf2be86138866e65b2
RyzeZhao <643080112@qq.com>
2019-03-07 10:34:29 +0800

    GreenPoint：测试方法

--- src/test/java/com/zhaolearn/green/GreenApplicationTests.java
+++ src/test/java/com/zhaolearn/green/GreenApplicationTests.java
@@ -1,16 +1,24 @@
 package com.zhaolearn.green;
 
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GreenApplicationTests {
-
+    private static GreenTools greenTools=new GreenTools();
     @Test
-    public void contextLoads() {
+    public void testClone() throws IOException, GitAPIException {
+        greenTools.testClone();
     }
-
+    @Test
+    public void testAdd() throws IOException, GitAPIException {
+        greenTools.testAdd("myfile.txt");
+    }
 }
